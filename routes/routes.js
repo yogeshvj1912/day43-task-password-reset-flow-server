d428c9a3ace7f0cc1cb1ead9203fc2b3187b72b8
yogeshvj1912 <113254846+yogeshvj1912@users.noreply.github.com>
2023-07-08 23:16:29 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -145,7 +145,7 @@ router.post("/sendpasswordlink",async(req,res)=>{
                 from:process.env.USER_EMAIL,
                 to:email,
                 subject:"Sending Email For password Reset",
-                text:`This Link Valid For 2 MINUTES http://localhost:3000/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
+                text:`This Link Valid For 2 MINUTES https://profound-sunburst-e217cc.netlify.app/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
             }
 
             transporter.sendMail(mailOptions,(error,info)=>{
